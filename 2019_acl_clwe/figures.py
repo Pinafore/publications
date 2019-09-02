e7296079d436bfec8c1ecc45c27e80d4d17f02ed
zmz <zhangmozhi@gmail.com>
2019-09-02 18:53:08 -0400

    Import 2019_acl_clwe

---
+++ 2019_acl_clwe/figures.py
@@ -0,0 +1,19 @@
+
+# Script to create plots for your paper
+import pandas as pd
+import numpy as np
+
+from plotnine import *
+
+palette = {2: ["#000000", "#CFB87C"],
+           3: ["#000000", "#FACD00", "#DA1D2B"],
+           4: ["#FACD00", "#CFB87C", "#b2df8a", "#33a02c"]}
+
+kROOT_DIR = "2019_acl_clwe"
+
+def datadir(filename):
+    return "%s/data/%s" % (kROOT_DIR, filename)
+
+def gfxdir(filename):
+    return "%s/auto_fig/%s" % (kROOT_DIR, filename)
+
