9a89ea20bfefbfb848333f89ac7e26162fea346f
Jordan Boyd-Graber <jbg@google.com>
2019-10-31 13:37:53 +0100

    Import of 2020_acl_trivia_tournament

--- scripts/sanitize.py
+++ scripts/sanitize.py
@@ -4,7 +4,7 @@ from glob import glob
 import re
 import sys
 
-kCOMMENT = re.compile(r"(?<!\\)%.*\n")
+kCOMMENT = re.compile(r"(?<!\\)%.*")
 kSTRIP = re.compile(r"\\.*comment{")
 strippable = set(["\\hidetext{", "\\ignore{"])
 
