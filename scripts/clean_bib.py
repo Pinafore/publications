80aae4f5341ffae58fa66a5501e3261da932dab9
Pedro Rodriguez <me@pedro.ai>
2021-08-17 07:21:07 -0700

    Import of 2021_acl_leaderboard

--- scripts/clean_bib.py
+++ scripts/clean_bib.py
@@ -1,6 +1,7 @@
 
 from collections import defaultdict
 import sys
+import argparse
 
 kUNACCEPTABLE_OUTPUT = {"inbook": set(["url"]),
                         "inproceedings": set(["isbn", "series", "pages", "month", "address", "location", "editor", "tagline", "abstract", "publisher", "url", "doi", "organization"]),
@@ -11,14 +12,22 @@ kUNACCEPTABLE_OUTPUT = {"inbook": set(["url"]),
                         "techreport": set([]),
                         "article": set(["issn", "doi", "url", "abstract"])}
 
+doi_fields = ["url", "doi", "eprint"]
+
 def bracket_count(s):
     return sum(1 for x in s if x=="{") - sum(1 for x in s if x=="}")
-    
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("old", type=str, help="Path to old bib file")
+    parser.add_argument("new", type=str, help="Path to new bib file")
+    parser.add_argument("--doi", action="store_true", help="Keep URLs/DOIs")
+    args = parser.parse_args()
+
     output = defaultdict(list)
     key = ""
     in_open_field = False
-    with open(sys.argv[1]) as infile:
+    with open(args.old) as infile:
         for ii in infile:
             if ii.startswith("@"):
                 kind = ii.split("{")[0][1:].lower()
@@ -35,7 +44,11 @@ if __name__ == "__main__":
             elif ii.startswith("%"):
                 continue
             elif "=" in ii:
-                if ii.split("=")[0].strip().lower() not in kUNACCEPTABLE_OUTPUT[kind]:
+                field = ii.split("=")[0].strip().lower()
+                acceptable = (kind not in kUNACCEPTABLE_OUTPUT) or \
+                    (field not in kUNACCEPTABLE_OUTPUT[kind])
+                doi_field = field in doi_fields and args.doi
+                if acceptable or doi_field:
                     output[(key, kind)].append(ii.strip())
                     if bracket_count(ii) > 0:
                         in_open_field = True
@@ -46,13 +59,13 @@ if __name__ == "__main__":
                 if key and ii.strip() != "}" and ii.strip():
                     print("Non-bibtex %s: %s" % (key, ii.strip()))
 
-    with open(sys.argv[2], 'w') as outfile:                    
+    with open(args.new, 'w') as outfile:
         for key, kind in sorted(output, key=lambda x: x[0].lower()):
             spacing = "".join([" "] * (len(kind) + 2))
             outfile.write("\n\n@%s{%s,\n" % (kind, key))
             for jj in output[(key, kind)]:
                 outfile.write("%s%s\n" % (spacing, jj.strip()))
             outfile.write("}")
-                
-            
-                
+
+
+
